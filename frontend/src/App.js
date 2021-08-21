d8dfa798dd9b69c9a44fd13eb2b0622594847cf5
Michal Pielaszkiewicz <michal.pielaszkiewicz@komputronik.pl>
2021-08-21 22:22:50 +0200

    frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import './components/Modules'
+import ExpenseItem from "./components/Modules";
+
+function App() {
+  return (
+    <ExpenseItem>Expense Item!</ExpenseItem>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Modules/bckg/Background.jsx
@@ -0,0 +1,5 @@
+import StyledExpenseItem, {background, b} from "./styles";
+export default function Background() {
+    return (<div><StyledExpenseItem>sssss</StyledExpenseItem>
+    <StyledParagraph>ddddd</StyledParagraph></div>)
+}
