a7fadc8ef61dca244933350386b31830e09476e2
yautay <yautay@gmail.com>
2021-09-11 19:16:43 +0200

    wip

--- backend/db/models/bgg_attributes.py
+++ backend/db/models/bgg_attributes.py
@@ -8,6 +8,6 @@ class BggAttributes(Base):
     id = Column(Integer, primary_key=True)
     attribute_bgg_index = Column(Integer, index=True, nullable=True)
     attribute_bgg_value = Column(String(250), nullable=True)
-    attribute_bgg_json = Column(JSON)
+    attribute_bgg_json = Column(JSON, nullable=True)
     bgg_game_attributes = relationship("BggGameAttributes")
 
